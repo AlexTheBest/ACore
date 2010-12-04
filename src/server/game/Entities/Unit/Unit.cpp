fbf57e8d51447c16192ed55f08b42f4773e4f29c
AlexTheBest <alex@hmele.com.au>
2010-12-04 14:10:22 +1100

    Fix

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9396,7 +9396,7 @@ bool Unit::Attack(Unit *victim, bool meleeAttack)
     //if (GetTypeId() == TYPEID_UNIT)
     //    this->ToCreature()->SetCombatStartPosition(GetPositionX(), GetPositionY(), GetPositionZ());
 
-    if (GetTypeId() == TYPEID_UNIT && !this->ToCreature()->IsPet())
+    if (GetTypeId() == TYPEID_UNIT && !this->ToCreature()->isPet())
     {
         // should not let player enter combat by right clicking target
         SetInCombatWith(victim);
